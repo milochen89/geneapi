c2df2573808a3253dc1d348993f829f2daff9860
chenhao <milochen89@yahoo.com>
2013-08-02 15:34:48 +0100

    upload outputfilelist

---
+++ src/main/java/com/ucl/genelab/metadata/Outputfilelist.java
@@ -0,0 +1,15 @@
+package com.ucl.genelab.metadata;
+
+import java.util.ArrayList;
+public class Outputfilelist {
+	private ArrayList<String> outputlist;
+
+	public ArrayList<String> getOutputlist() {
+		return outputlist;
+	}
+
+	public void setOutputlist(ArrayList<String> outputlist) {
+		this.outputlist = outputlist;
+	}
+	
+}
